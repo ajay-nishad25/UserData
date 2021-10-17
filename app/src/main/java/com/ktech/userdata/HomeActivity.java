a7ebbf17611ebdfe79e0197b21f7831e29a62133
AjayBholaNishad <ajaynishad4123@gmail.com>
2021-10-17 15:09:40 +0530

    implement camera code

--- app/src/main/java/com/ktech/userdata/HomeActivity.java
+++ app/src/main/java/com/ktech/userdata/HomeActivity.java
@@ -1,19 +1,44 @@
 package com.ktech.userdata;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class HomeActivity extends AppCompatActivity {
 
+    public static final int CAMERA_PERMISSION_CODE = 100;
+    public static final int CAMERA_REQUEST_CODE = 101;
+
+    ImageView aUserImage;
     EditText aName, aEmail, aMobileNo, aRikshawNo, aAddress;
     TextView aAddPhoto, aClearData;
     Button aUpload, aViewDatabase;
+
+    String currentPhotoPath;
 
 
     @Override
@@ -21,13 +46,113 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        // opening ViewActivity
-        aViewDatabase.setOnClickListener(new View.OnClickListener() {
+        //Reference
+        aUserImage = findViewById(R.id.userImage_imageView_aHome);
+        aName = findViewById(R.id.name_editText_aHome);
+        aEmail = findViewById(R.id.email_editText_aHome);
+        aMobileNo = findViewById(R.id.mobileNo_editText_aHome);
+        aRikshawNo = findViewById(R.id.rikshawNumber_editText_aHome);
+        aAddress = findViewById(R.id.address_EditText_aHome);
+        aAddPhoto = findViewById(R.id.addPhoto_textView_aHome);
+        aClearData = findViewById(R.id.clearAboveData_textView_aHome);
+        aUpload = findViewById(R.id.upload_Button_aHome);
+        aViewDatabase = findViewById(R.id.viewDatabase_Button_aHome);
+
+        //Capturing user image
+        aAddPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(HomeActivity.this , ViewDatabaseActivity.class));
+                askCameraPermission();
             }
         });
 
+
+        // opening ViewDatabaseActivity
+        aViewDatabase.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HomeActivity.this, ViewDatabaseActivity.class));
+            }
+        });
+
+    }
+
+    private void askCameraPermission() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_CODE);
+        } else {
+                dispatchTakePictureIntent();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+               dispatchTakePictureIntent();
+            }else {
+                Toast.makeText(getApplicationContext(), "Permission Required", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (resultCode == Activity.RESULT_OK) {
+                File f = new File(currentPhotoPath);
+                aUserImage.setVisibility(View.VISIBLE);
+                aUserImage.setImageURI(Uri.fromFile(f));
+                Log.d("tag", "Absolute Url of Image is : " + Uri.fromFile(f));
+
+            }
+
+        }
+
+    }
+
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
     }
 }
